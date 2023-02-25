1783418a7146c6ff01e88c1c8c034d8b1c90a5f8
Mark <nedzelsky.mark@mail.ru>
2023-02-26 00:59:48 +0300

    all done

--- Program.cs
+++ Program.cs
@@ -9,3 +9,29 @@
 
     return wordsArray;
 }
+
+string[] MakeSmallerArray(string[] array)
+{
+    string smallWords ="";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            smallWords += $" {array[i]}";
+        }
+    }
+
+    string[] newArray = smallWords.Split(" ");
+
+    return newArray;
+}
+
+string[] wordsArray = GetArray("Для создания массива введите слова разделяя их знаком пробела.");
+string[] smallWordsArray = MakeSmallerArray(wordsArray);
+
+Console.WriteLine("Новый массив из строк состоящих не более чем из 3 символов:");
+for (int i = 0; i < smallWordsArray.Length; i++)
+{
+    Console.Write($"{smallWordsArray[i]} ");
+}
